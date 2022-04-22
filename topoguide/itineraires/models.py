46e36ca5119322e7fdff8779ead4c1d255d0e1bc
BFHENG <baptiste.heng.s@gmail.com>
2022-04-22 15:13:30 +0200

    test baptiste

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -3,7 +3,7 @@ from django.db.models import IntegerField, Model
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+#ESSAI DE COMMIT
 class Itineraire(models.Model):
     """
     Un itinéraire constitué du titre, du point de départ, de la description, de l'altitude de départ, 
