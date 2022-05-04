dcd0e89ebe8068dc9efe318ea63f3acdaebdbd5d
Hakan ACIYAN <hakan.aciyan@student.isae-supaero.fr>
2022-05-04 17:31:49 +0200

    Configuration en en-us

--- topoguide/topoguide/settings.py
+++ topoguide/topoguide/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
-    'leaflet'
+    'bootstrap5'
     
 ]
 
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'fr-fr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Paris'
 
